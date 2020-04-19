53a7494e7a6fe70b972074eecfd5c82520980035
BjornM111 <malmgren.bjorn@gmail.com>
2020-04-19 09:52:36 +0200

    react_qt:   support for layouts in layouts   better handling of signals   fixed text cursors for QLineEdit

--- react_qt.py
+++ react_qt.py
@@ -1,4 +1,5 @@
 from PySide2.QtCore import SignalInstance
+from PySide2.QtWidgets import QWidget, QLayout, QLineEdit, QTextEdit
 
 from react import Renderer
 
@@ -26,18 +27,37 @@ class QtRenderer(Renderer):
         element.item.takeAt(i)
 
     def _insert(self, parent, i, element):
-        parent.item.insertWidget(i, element.item)
+        if isinstance(element.item, QWidget):
+            parent.item.insertWidget(i, element.item)
+        elif isinstance(element.item, QLayout):
+            parent.item.insertLayout(i, element.item)
+        else:
+            raise NotImplementedError(
+                "type for {} is not supported yet".format(element.item)
+            )
 
     def _setprops(self, element, props, old=None):
+        meta = element.item.metaObject()
         for key, value in props.items():
-            if  key != "visible" and isinstance(getattr(element.item, key), SignalInstance):
-                signal = getattr(element.item, key)
-                if old and key in old.props:
-                    signal.disconnect(old.props[key])
-                signal.connect(value)
+            # we need to take care of text cursors as they are normally cleared
+            # when the text us updated
+            if  key == "text" and isinstance(element.item, QLineEdit):
+                cursor = element.item.cursorPosition()
+                element.item.setText(value)
+                if cursor:
+                    element.item.setCursorPosition(
+                        min(cursor, len(value)),
+                    )
                 continue
-            setter = getattr(element.item, 'set'+key[0].upper()+key[1:])
-            setter(value)
+
+            if  meta.indexOfProperty(key) != -1:
+                element.item.setProperty(key, value)
+                continue
+
+            signal = getattr(element.item, key)
+            if old and old.props.get(key):
+                signal.disconnect(old.props[key])
+            signal.connect(value)
 
     def _setlayout(self, element, layout):
         element.item.setLayout(layout.item)
