b8258d027145234f2ff21c1e78c0d72f2e770a64
BjornM111 <malmgren.bjorn@gmail.com>
2020-04-19 09:53:29 +0200

    chat.py: another example app for testing text fields

---
+++ chat.py
@@ -0,0 +1,64 @@
+import sys
+
+from PySide2 import QtWidgets
+
+from wrapper import wrap
+from react import Element
+from react_qt import QtRenderer, style
+
+wrap('QtWidgets', QtWidgets, Element)
+from wrapper.QtWidgets import (
+    QApplication,
+    QDialog,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QLabel,
+    QTextEdit,
+    QLineEdit,
+    QWidget,
+)
+
+
+def master(use_state):
+    messages, set_messages = use_state([])
+    message, set_message = use_state("")
+
+    return QDialog(
+        layout=QVBoxLayout(
+            widgets=[
+                QTextEdit(
+                    plainText="\n".join(messages),
+                ),
+                QHBoxLayout(
+                    widgets=[
+                        QLineEdit(
+                            text=message,
+                            textEdited=lambda x: set_message(x),
+                        ),
+                        QPushButton(
+                            text=">",
+                            styleSheet=style(font_weight="bold"),
+                            pressed=lambda: (
+                                (
+                                    set_messages([*messages, message]),
+                                    set_message(""),
+                                ) if message else None
+                            ),
+                        ),
+                    ]
+                ),
+            ]
+        )
+    )
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    renderer = QtRenderer()
+
+    renderer.render(Element(master))
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
